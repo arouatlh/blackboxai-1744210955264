de841020e3606261c7f9eb0cc01184a8e63a98d9
arouatlh <155142520+arouatlh@users.noreply.github.com>
2025-04-09 23:02:50 +0800

    Add src/components/Menu.js 1744210967044

---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Menu() {
+  return (
+    <div>
+      <h2>Menu Page</h2>
+      {/* Menu functionality will be implemented here */}
+    </div>
+  );
+}
+
+export default Menu;
